98d3c70c2e7dbaf4321ec2db6461f363b566fbf6
kunal <thakurrajput98765@gmail.com>
2022-06-21 09:41:18 +0530

    updated files

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ const toc = document.querySelector('.toc');
 
 toc.addEventListener('click', e => {
     if(e.target.classList.contains('list-item')){
-        e.target.querySelector('.child-list').style.display = 'none';
         const ul = e.target.closest('.list')
         const siblings = ul.querySelectorAll('.list-item')
 
         siblings.forEach( el => {
             if(el != e.target){
                 el.querySelector('.child-list').style.display = 'none';
-                el.querySelector('.arrows').classList.remove('active')
+                el.querySelector('.arrows').classList.remove('active');
                 el.style.color = '#6d6d6d'
             }
             else {
-                el.querySelector('.child-list').style.display = 'block';
-                el.querySelector('.arrows').classList.add('active')
-                el.style.color = 'black'
+                el.style.color = 'black';
+                
+                if(el.querySelector('.arrows').classList.contains('active')) {
+                    el.querySelector('.child-list').style.display = 'none';
+                    el.querySelector('.arrows').classList.remove('active');
+                    el.style.color = '#6d6d6d';
+                }
+                else {
+                    el.querySelector('.child-list').style.display = 'block';
+                    el.querySelector('.arrows').classList.add('active');
+                }
+                
             }
         });
     }
